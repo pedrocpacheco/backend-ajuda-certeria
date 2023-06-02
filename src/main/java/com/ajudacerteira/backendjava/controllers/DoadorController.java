da8967c727893d48839a35a6eff5eca2495ff085
pedrocpacheco <cpedropacheco@hotmail.com>
2023-06-02 00:08:02 -0300

    adicionando cors

--- src/main/java/com/ajudacerteira/backendjava/controllers/DoadorController.java
+++ src/main/java/com/ajudacerteira/backendjava/controllers/DoadorController.java
@@ -3,6 +3,7 @@ package com.ajudacerteira.backendjava.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import com.ajudacerteira.backendjava.entities.Doador;
 import com.ajudacerteira.backendjava.repositories.DoadorRepository;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class DoadorController {
     
     @Autowired
