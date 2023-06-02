d6e1dd452fd32c3d8943395369fa523cb174baf7
pedrocpacheco <cpedropacheco@hotmail.com>
2023-06-01 23:54:58 -0300

    trying to make the post method work

--- src/main/java/com/ajudacerteira/backendjava/controllers/DoadorController.java
+++ src/main/java/com/ajudacerteira/backendjava/controllers/DoadorController.java
@@ -7,33 +7,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ajudacerteira.backendjava.entities.Doador;
 import com.ajudacerteira.backendjava.repositories.DoadorRepository;
 
 @RestController
-@RequestMapping(value = "/doadores")
 public class DoadorController {
     
     @Autowired
     private DoadorRepository repository;
 
-    @GetMapping
+    @GetMapping("/doadores")
     public List<Doador> findAll(){
         return repository.findAll();
     }
 
-    @GetMapping(value = "/{id}")
+    @GetMapping(value = "/doadores/{id}")
     public Doador findById(@PathVariable Long id){
         return repository.findById(id).get();
     }
 
-    @PostMapping
+    @PostMapping("/doadores")
     public Doador saveDoador(@RequestBody Doador doador){
         Doador save = repository.save(doador);
         return save;
     }
-
 }
