2ef0a06097532f8b4e6d8ea281d487cb8493a50b
pedrocpacheco <cpedropacheco@hotmail.com>
2023-06-05 22:10:22 -0300

    services

--- src/main/java/com/ajudacerteira/backendjava/controllers/OngController.java
+++ src/main/java/com/ajudacerteira/backendjava/controllers/OngController.java
@@ -3,7 +3,6 @@ package com.ajudacerteira.backendjava.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,44 +11,40 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.ajudacerteira.backendjava.entities.Ong;
-import com.ajudacerteira.backendjava.repositories.OngRepository;
+import com.ajudacerteira.backendjava.services.OngService;
 
 @RestController
 @CrossOrigin(origins = "*")
 public class OngController {
     
     @Autowired
-    private OngRepository repository;
+    private OngService service;
 
     @GetMapping("/ongs")
     public List<Ong> findAll(){
-        return repository.findAll();
+        return service.findAll();
     }
 //
     @GetMapping(value = "/ongs/{id}")
     public Ong findById(@PathVariable Long id){
-        return repository.findById(id).get();
+        return service.findById(id);
     }
 
     @PostMapping("/ongs")
     public Ong saveOng(@RequestBody Ong ong){
-        return repository.save(ong);
+        return service.saveOng(ong);
     }
 
     @PutMapping("/ongs/{id}")
     public Ong updateOng(@PathVariable Long id, @RequestBody Ong ong){
-        if(!repository.existsById(id))
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        ong.setId(id);
-        return repository.save(ong);
+        return service.updateOng(id, ong);
     }
 
     @DeleteMapping("/ongs/{id}")
     public void deleteOng(@PathVariable Long id){
-        repository.deleteById(id);
+        service.deleteOng(id);
     }
 }
 
