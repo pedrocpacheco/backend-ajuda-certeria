4c9160ec7436ed91c033cbd3216baaf8f119079a
pedrocpacheco <cpedropacheco@hotmail.com>
2023-06-05 21:56:05 -0300

    vai logo por favor

--- src/main/java/com/ajudacerteira/backendjava/controllers/DoadorController.java
+++ src/main/java/com/ajudacerteira/backendjava/controllers/DoadorController.java
@@ -3,6 +3,7 @@ package com.ajudacerteira.backendjava.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,39 +12,43 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.ajudacerteira.backendjava.entities.Doador;
-import com.ajudacerteira.backendjava.services.DoadorService;
+import com.ajudacerteira.backendjava.repositories.DoadorRepository;
 
 @RestController
 @CrossOrigin(origins = "*")
 public class DoadorController {
     
     @Autowired
-    private DoadorService service;
+    private DoadorRepository repository;
 
     @GetMapping
     public List<Doador> findAll(){
-        return service.findAll();
+        return repository.findAll();
     }
 
     @GetMapping(value = "/{id}")
     public Doador findById(@PathVariable Long id){
-        return service.findById(id);
+        return repository.findById(id).get();
     }
 
     @PostMapping
     public Doador saveDoador(@RequestBody Doador doador){
-        return service.saveDoador(doador);
+        return repository.save(doador);
     }
 
     @PutMapping(value = "/{id}")
     public Doador updateDoador(@PathVariable Long id, @RequestBody Doador doador){
-        return service.updateDoador(id, doador);
+        if(!repository.existsById(id))
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        doador.setId(id);
+        return repository.save(doador);
     }
 
     @DeleteMapping(value = "/{id}")
     public void deleteDoador(@PathVariable Long id){
-        service.deleteDoador(id);
+        repository.deleteById(id);
     }
 }
